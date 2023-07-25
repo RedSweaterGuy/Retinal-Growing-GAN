18d4d03f324332898067217805388eb9f029f8f1
Mark-Christian Bront <61542842+RedSweaterGuy@users.noreply.github.com>
2023-07-25 16:24:18 +0200

    changed training rounds used for each growing epoch and split the code for the original and growing gan onto two different functions for training

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def main():
     #runs.append({'dataset': 'DRIVE', 'data_rotation': 3, 'model': 'original', 'n_rounds': 10})
     #runs.append({'dataset': 'DRIVE', 'data_rotation': 36, 'model': 'original', 'n_rounds': 10})
     #runs.append({'dataset': 'DRIVE', 'data_rotation': 180, 'model': 'original', 'n_rounds': 10})
-    runs.append({'dataset': 'DRIVE', 'data_rotation': 180, 'model': 'growing', 'n_rounds': 10, 'grow_round_multiplier': 1})
+    #runs.append({'dataset': 'DRIVE', 'data_rotation': 180, 'model': 'growing', 'n_rounds': 10})
+    runs.append({'dataset': 'DRIVE', 'data_rotation': 180, 'model': 'growing', 'n_rounds': [40,20,15,10]})
     for run in runs:
         print(f"starting run: {run}")
         #img_out_dir = gen_data(run)
