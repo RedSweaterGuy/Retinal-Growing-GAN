1afe57ea2936cdc136519f565f7fc08e6bcbfbbc
Mark-Christian Bront <61542842+RedSweaterGuy@users.noreply.github.com>
2023-07-28 14:24:26 +0200

    updated visualization to have gradients in the ground truth VS GAN result

--- viz_res_demo.py
+++ viz_res_demo.py
@@ -7,7 +7,9 @@ import tkinter as tk
 from tkinter import filedialog
 from skimage.transform import resize
 from skimage.morphology import dilation
-from matplotlib.colors import ListedColormap
+from matplotlib.colors import ListedColormap, LinearSegmentedColormap
+from matplotlib.colorbar import ColorbarBase
+import palettable
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -16,16 +18,32 @@ rotate_by = 0
 query_ground_truth = True
 _cmap = "gray"
 
-
 # maybe change this later
-custom_colors = [
+custom_colors2 = [
+    (0, 0, 0),  # black
+    (140, 81, 10),  # brown
+    (191, 129, 45),
+    (223, 194, 125),
+    (246, 232, 195),
+    (255, 255, 255),  # white
+    (199, 234, 229),
+    (128, 205, 193),
+    (53, 151, 143),
+    (1, 102, 94)  # aquamarine
+]
+custom_colors1 = [
     "black",  # Black if image1 == image2 and image1 == 0
     "white",  # White if image1 == image2 and image1 == 1
-    "orange",  # Red for image1 > image2
-    "red"  # Green for image2 > image1
+    "orange",  # Orange for image1 > image2
+    "red"  # Red for image2 > image1
 ]
+custom_color_false_positive = (197, 27, 125)
+custom_color_false_negative = (77, 146, 33)
+custom_color_true_positive = (255,255,255)
+background_color = (0,0,0)
+
 # Create the custom colormap
-custom_cmap = ListedColormap(custom_colors)
+custom_cmap = ListedColormap([custom_color_false_positive, custom_color_true_positive, custom_color_false_negative])
 
 dataset = "DRIVE"
 methods = ['origGAN', 'growGAN', 'growGAN20']
@@ -68,98 +86,97 @@ for i in range(len(methods)):
 root = tk.Tk()
 root.withdraw()
 
-while True:
-    segmentations = dict()  # dict where all segmentations for one run will be stored
-    # first plot
-    figs, axs = plt.subplots(2, len(methods) + (1 * query_ground_truth), sharex=True, sharey=True)
-    for ax_s in axs:
-        for ax in ax_s:
-            ax.set_yticklabels([])
-            ax.set_xticklabels([])
-            ax.set_xticks([])
-            ax.set_yticks([])
-    image_path = filedialog.askopenfilename()
+segmentations = dict()  # dict where all segmentations for one run will be stored
+# first plot
+figs, axs = plt.subplots(2, len(methods) + (1 * query_ground_truth), sharex=True, sharey=True)
+for ax_s in axs:
+    for ax in ax_s:
+        ax.set_yticklabels([])
+        ax.set_xticklabels([])
+        ax.set_xticks([])
+        ax.set_yticks([])
+image_path = filedialog.askopenfilename()
 
-    img = Image.open(image_path)
-    if rotate_by > 0:
-        img = img.rotate(rotate_by)
-    img = np.asarray(img).astype(np.float32)
-    # set test dataset
-    test_imgs = resize(img, img_size)[None, :, :, :]
-    mean = np.mean(test_imgs[test_imgs[..., 0] > 40.0], axis=0, dtype=np.float32)
-    std = np.std(test_imgs[test_imgs[..., 0] > 40.0], axis=0, dtype=np.float32)
-    assert len(mean) == 3 and len(std) == 3
-    orig_imgs = np.asarray(test_imgs) / 255
-    orig_copy = np.copy(orig_imgs)
-    test_imgs[0, ...] = (test_imgs[0, ...] - mean) / std
+img = Image.open(image_path)
+if rotate_by > 0:
+    img = img.rotate(rotate_by)
+img = np.asarray(img).astype(np.float32)
+# set test dataset
+test_imgs = resize(img, img_size)[None, :, :, :]
+mean = np.mean(test_imgs[test_imgs[..., 0] > 40.0], axis=0, dtype=np.float32)
+std = np.std(test_imgs[test_imgs[..., 0] > 40.0], axis=0, dtype=np.float32)
+assert len(mean) == 3 and len(std) == 3
+orig_imgs = np.asarray(test_imgs) / 255
+orig_copy = np.copy(orig_imgs)
+test_imgs[0, ...] = (test_imgs[0, ...] - mean) / std
 
-    ground_truth_path = image_path.replace("images", "1st_manual").replace("_test", "_manual1")
-    if "DRIVE" in ground_truth_path:
-        ground_truth_path = ground_truth_path.replace(".tif", ".gif")
-    else:
-        ground_truth_path = ground_truth_path.replace(".ppm", ".ah.ppm")
-    ground_truth_img = Image.open(ground_truth_path)
-    if rotate_by > 0:
-        ground_truth_img = ground_truth_img.rotate(rotate_by)
-    ground_truth_img = np.asarray(ground_truth_img).astype(np.float32)
-    ground_truth_img = resize(ground_truth_img, img_size)
-    segmentations['ground_truth_img'] = ground_truth_img
-    axs[0, 0].imshow(ground_truth_img, cmap=_cmap)
+ground_truth_path = image_path.replace("images", "1st_manual").replace("_test", "_manual1")
+if "DRIVE" in ground_truth_path:
+    ground_truth_path = ground_truth_path.replace(".tif", ".gif")
+else:
+    ground_truth_path = ground_truth_path.replace(".ppm", ".ah.ppm")
+ground_truth_img = Image.open(ground_truth_path)
+if rotate_by > 0:
+    ground_truth_img = ground_truth_img.rotate(rotate_by)
+ground_truth_img = np.asarray(ground_truth_img).astype(np.float32) / 255
+ground_truth_img = resize(ground_truth_img, img_size)
+segmentations['ground_truth_img'] = ground_truth_img
+axs[0, 0].imshow(ground_truth_img, cmap=_cmap)
 
-    axs[1, 0].imshow(remain_in_mask(orig_imgs[0], multi_dilation(ground_truth_img, cross, 4)), cmap=_cmap)
+axs[1, 0].imshow(remain_in_mask(orig_imgs[0], multi_dilation(ground_truth_img, cross, 4)), cmap=_cmap)
+if show_titles:
+    axs[0, 0].set_title("Ground truth")
+
+for i in range(len(methods)):
+    generated = models[i].predict(test_imgs, batch_size=batch_size)
+    generated = np.squeeze(generated, axis=3)[0]
+    segmentations[methods[i]] = generated
+    axs[0, i + (1 * query_ground_truth)].imshow(generated, cmap=_cmap)
+    orig_imgs = np.copy(orig_copy)
+    axs[1, i + (1 * query_ground_truth)].imshow(remain_in_mask(orig_imgs[0], multi_dilation(generated, cross, 4)),
+                                                cmap=_cmap)
+
     if show_titles:
-        axs[0, 0].set_title("Ground truth")
+        axs[0, i + (1 * query_ground_truth)].set_title(titles[i])
+plt.tight_layout()
+plt.show()
 
-    for i in range(len(methods)):
-        generated = models[i].predict(test_imgs, batch_size=batch_size)
-        generated = np.squeeze(generated, axis=3)[0]
-        segmentations[methods[i]] = generated
-        axs[0, i + (1 * query_ground_truth)].imshow(generated, cmap=_cmap)
-        orig_imgs = np.copy(orig_copy)
-        axs[1, i + (1 * query_ground_truth)].imshow(remain_in_mask(orig_imgs[0], multi_dilation(generated, cross, 4)),
-                                                    cmap=_cmap)
+# second plot
+figs, axs = plt.subplots(1, len(methods) + (1 * query_ground_truth), sharex=True, sharey=True)
 
-        if show_titles:
-            axs[0, i + (1 * query_ground_truth)].set_title(titles[i])
-    plt.tight_layout()
-    plt.show()
+for ax_s in axs:
+    # for ax in ax_s:
+    ax_s.set_yticklabels([])
+    ax_s.set_xticklabels([])
+    ax_s.set_xticks([])
+    ax_s.set_yticks([])
 
-    # second plot
-    figs, axs = plt.subplots(2, len(methods) + (1 * query_ground_truth), sharex=True, sharey=True)
+if query_ground_truth:
+    axs[0].imshow(ground_truth_img, cmap=_cmap)
+    if show_titles:
+        axs[0].set_title("Ground truth")
+for i in range(len(methods)):
 
-    for ax_s in axs:
-        for ax in ax_s:
-            ax.set_yticklabels([])
-            ax.set_xticklabels([])
-            ax.set_xticks([])
-            ax.set_yticks([])
+    fake = segmentations[methods[i]].copy()
+    grd = segmentations['ground_truth_img'].copy()
+    _output = grd - fake
 
-    if query_ground_truth:
-        axs[0, 0].imshow(ground_truth_img, cmap=_cmap)
+    diff_img = np.zeros((_output.shape[0],_output.shape[1],3), dtype=int)
+    _output[np.logical_and(_output>0, _output<0.1)] = 0
+    _output[np.logical_and(_output<0, _output>-0.1)] = 0
+    aux = Image.fromarray(_output)
 
-        axs[1, 0].imshow(remain_in_mask(orig_imgs[0], ground_truth_img), cmap=_cmap)
-        if show_titles:
-            axs[0, 0].set_title("Ground truth")
-    for i in range(len(methods)):
-        fake = np.where(segmentations[methods[i]].copy() > 0.1,1,0)
-        grd = np.where(segmentations['ground_truth_img'].copy() >0.1,1,0)
-        #_difference = np.logical_xor(grd.astype(bool), fake.astype(bool))    #np.where(grd - fake > 0, 1, 0)
-        _output = np.zeros(grd.shape)
+    diff_img[_output<0,:] = np.transpose((197 * abs(_output[_output<0]),27 * abs(_output[_output<0]),125* abs(_output[_output<0])))
+    diff_img[_output>0,:] = np.transpose((custom_color_false_negative[0] * _output[_output > 0], custom_color_false_negative[1] * _output[_output > 0], custom_color_false_negative[2] * _output[_output > 0]))
+    diff_img[np.logical_and(_output==0,grd>0.1),:] = custom_color_true_positive
 
-        _output[fake < grd] = 2
-        _output[fake > grd] = 3
-        _output[fake * grd > 0] = 1
+    orig_imgs = np.copy(orig_copy)
+    axs[i + (1 * query_ground_truth)].set_facecolor(background_color)
 
-        orig_imgs = np.copy(orig_copy)
-        axs[0, i + (1 * query_ground_truth)].imshow(_output, cmap=custom_cmap, interpolation='nearest')
-        axs[1, i + (1 * query_ground_truth)].imshow(
-            remain_in_mask(orig_imgs[0], multi_dilation(segmentations[methods[i]], cross, 4)),
-            cmap=_cmap)
+    axs[i + (1 * query_ground_truth)].imshow(diff_img)
 
-        if show_titles:
-            axs[0, i + (1 * query_ground_truth)].set_title(f"ground truth VS {titles[i]}")
 
-    plt.tight_layout()
-    plt.show()
-    if input("Exit? [Y]/N ") != "N":
-        break  # exit(0)
+    if show_titles:
+        axs[i + (1 * query_ground_truth)].set_title(f"Ground truth VS {titles[i]}")
+plt.tight_layout()
+plt.show()
